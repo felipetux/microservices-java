5d633f3f5d02c146767592451a87c4c18496cbd4
felipetux <felipe.tux@gmail.com>
2018-10-19 00:29:46 -0300

    Update Test.java

--- src/main/java/io/veicot/openshift/rest/Test.java
+++ src/main/java/io/veicot/openshift/rest/Test.java
@@ -6,6 +6,7 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import java.util.Date;
 
 
 @Path("/test")
@@ -14,6 +15,10 @@ public class Test extends Application {
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Test from Openshift ").build();
+		return Response.ok(dateFormat.format(date)).build();
+		
+		//DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		//Date date = new Date();
+		//System.out.println(dateFormat.format(date));
 	}
 }
